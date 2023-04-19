07c4f664179100ce2e5ff7706b7a6188f7831d9e
GuillaumeVDN <vandenneucker.guillaume@gmail.com>
2023-04-19 10:41:39 +0200

    LineRenderer as a param in raycast

--- Assets/Scripts/Menu/MenuLevel.cs
+++ Assets/Scripts/Menu/MenuLevel.cs
@@ -3,6 +3,7 @@
 public class MenuLevel : InteractableObject {
     
     [SerializeField] public Vector3 TeleportPosition;
+    [SerializeField] public Vector3 TeleportRotation;
     
     public override void OnInteract() {
         Menu.instance.SelectedLevel = this;
--- Assets/Scripts/Menu/MenuTeleporter.cs
+++ Assets/Scripts/Menu/MenuTeleporter.cs
@@ -5,8 +5,12 @@ public class MenuTeleporter : InteractableObject {
     [SerializeField] private CharacterController Player;
     
     public override void OnInteract() {
-        if (Menu.instance.SelectedLevel)
-            Player.transform.position = Menu.instance.SelectedLevel.TeleportPosition;
+        if (Menu.instance.SelectedLevel) {
+            Player.transform.SetPositionAndRotation(
+                Menu.instance.SelectedLevel.TeleportPosition,
+                Quaternion.Euler(Menu.instance.SelectedLevel.TeleportRotation)
+            );
+        }
     }
     
 }
