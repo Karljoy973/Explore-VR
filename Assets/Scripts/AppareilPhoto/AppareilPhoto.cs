bb7f4f78c133bd9af645f9646861f4e0e44d62d6
Tomspace900 <tomspace.800@gmail.com>
2023-04-22 13:58:37 +0200

    Picture sound 🎵

--- Assets/Scripts/AppareilPhoto/AppareilPhoto.cs
+++ Assets/Scripts/AppareilPhoto/AppareilPhoto.cs
@@ -11,11 +11,11 @@ public class AppareilPhoto : InteractableObject {
     [Header("Player")]
     [SerializeField] private FirstPersonController Player;
     [SerializeField] private Camera PlayerCamera;
-    
+
     [Header("Appareil Photo")]
     [SerializeField] private Camera AppareilPhotoCamera;
     [SerializeField] private Transform CameraCarryPointParent;
-    
+
     [Header("UI and Camera Live Feed")]
     [SerializeField] private Image UIDot;
     [SerializeField] public TMP_Text TextInteractTogglePictureMode;
@@ -26,33 +26,40 @@ public class AppareilPhoto : InteractableObject {
 
     [NonSerialized] public Vector3 InitialPosition;
     [NonSerialized] public Quaternion InitialRotation;
-    
+
     private string _textInteractTogglePictureModeContents;
     private string _textInteractTakePictureContents;
     private string _textControlsContents;
-    
+
+    [SerializeField] private AudioClip pictureSound;
+    private AudioSource _audioSource;
+
     private void Start() {
         instance = this;
-        
+
         InitialPosition = transform.position;
         InitialRotation = transform.rotation;
-        
+
         _textInteractTogglePictureModeContents = TextInteractTogglePictureMode.text;
         _textInteractTakePictureContents = TextInteractTakePicture.text;
         _textControlsContents = TextControls.text;
         TextInteractTogglePictureMode.text = "";
         TextInteractTakePicture.text = "";
+
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = pictureSound;
     }
 
+
     public override void OnInteract() {  // on interact, pick up camera
-        // teleport so player carries it
+                                         // teleport so player carries it
         DataStore.instance.PickedUpAppareilPhoto = true;
         transform.parent = CameraCarryPointParent;
         transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
 
         // enable livestream to camera object
         EnableLiveStreamToCameraScreen();
-        
+
         // show UI
         TextInteractTogglePictureMode.text = _textInteractTogglePictureModeContents;
     }
@@ -64,23 +71,22 @@ public class AppareilPhoto : InteractableObject {
     private void Update() {
         if (!DataStore.instance.PickedUpAppareilPhoto)
             return;
-        
+
         // toggle picture mode
         if (Input.GetMouseButtonDown(1)) {
             var pictureMode = !DataStore.instance.PictureMode;
             DataStore.instance.PictureMode = pictureMode;
-            
+
             Player.enabled = !pictureMode;
             PlayerCamera.depth = pictureMode ? 0 : 1;  // depth 1 = set as main camera
             AppareilPhotoCamera.depth = pictureMode ? 1 : 0;  // depth 1 = set as main camera
             UIDot.enabled = !pictureMode;
-            
+
             if (pictureMode) {
                 AppareilPhotoCamera.targetTexture = null;  // disable livestream to camera object
                 TextInteractTakePicture.text = _textInteractTakePictureContents;
                 TextControls.text = "";
-            }
-            else {
+            } else {
                 EnableLiveStreamToCameraScreen();  // enable livestream to camera object
                 TextInteractTakePicture.text = "";
                 TextControls.text = _textControlsContents;
@@ -88,22 +94,22 @@ public class AppareilPhoto : InteractableObject {
         }
         // take picture in picture mode
         else if (DataStore.instance.PictureMode && Input.GetKeyDown(KeyCode.Space)) {
-            
+
             // disable UI texts
             TextInteractTakePicture.text = "";
             TextControls.text = "";
             TextInteractTogglePictureMode.text = "";
-            
+
             // render camera in new texture once and save screenshot
             var renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
             AppareilPhotoCamera.targetTexture = renderTexture;
             AppareilPhotoCamera.Render();
-            
+
             var screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
             RenderTexture.active = renderTexture;
             screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
             screenshot.Apply();
-            
+
             // save screenshot and show on displayer
             DataStore.instance.Pictures.Add(screenshot.EncodeToPNG());
             DataStore.instance.PictureIndex = DataStore.instance.Pictures.Count - 1;
@@ -111,18 +117,19 @@ public class AppareilPhoto : InteractableObject {
 
             // put camera back
             AppareilPhotoCamera.targetTexture = null;
-            
+
             // put UI back
             TextInteractTakePicture.text = _textInteractTakePictureContents;
             TextInteractTogglePictureMode.text = _textInteractTogglePictureModeContents;
-            
-            // TODO : play picture sound here
+
+            // play sound
+            _audioSource.Play();
         }
     }
 
     private void EnableLiveStreamToCameraScreen() {
         var rect = CameraLiveFeedTarget.rectTransform.rect;
-        var renderTexture = new RenderTexture((int) (CameraLiveFeedResolution * rect.width), (int) (CameraLiveFeedResolution * rect.height), 24);
+        var renderTexture = new RenderTexture((int)(CameraLiveFeedResolution * rect.width), (int)(CameraLiveFeedResolution * rect.height), 24);
         AppareilPhotoCamera.targetTexture = renderTexture;
         CameraLiveFeedTarget.texture = renderTexture;
     }
