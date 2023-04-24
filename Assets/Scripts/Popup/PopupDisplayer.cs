ea88816c53dd07067ceeda7c7330a2dc4393817c
Karl-Maison <karljoyeux@gmail.com>
2023-04-24 15:21:03 +0200

    improve popup

--- Assets/Scripts/Popup/PopupDisplayer.cs
+++ Assets/Scripts/Popup/PopupDisplayer.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PopupDisplayer : InteractableObject {
+public class PopupDisplayer : MonoBehaviour {
 
     public static PopupDisplayer instance;
     public void Start() => instance = this;
 
-    [SerializeField] private Renderer PopupRenderer;
+    [SerializeField] private Image PopupImage;
 
     public void ShowCurrentPopup() {
         if (DataStore.instance.SelectedPopup == null) {
-            PopupRenderer.enabled = false;
+            PopupImage.enabled = false;
         } else {
-            PopupRenderer.material = DataStore.instance.SelectedPopup.PopupMaterial;
-            PopupRenderer.enabled = true;
+            Texture2D texture = DataStore.instance.SelectedPopup.PopupTexture2D;
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            PopupImage.sprite = sprite;
+            PopupImage.enabled = true;
         }
     }
     
--- Assets/Scripts/Popup/PopupSelector.cs
+++ Assets/Scripts/Popup/PopupSelector.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PopupSelector : InteractableObject {
 
-    [SerializeField] public Material PopupMaterial;
+    [SerializeField] public Texture2D PopupTexture2D;
 
     public override void OnInteract() {
         DataStore.instance.SelectedPopup = DataStore.instance.SelectedPopup != this ? this : null;
