c359a4d76d07597cd0d3b895f653605dc0046976
GuillaumeVDN <vandenneucker.guillaume@gmail.com>
2023-04-24 15:44:32 +0200

    Can now get out of UI popup

--- Assets/Scripts/Popup/PopupDisplayer.cs
+++ Assets/Scripts/Popup/PopupDisplayer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,26 @@ public class PopupDisplayer : MonoBehaviour {
 
     [SerializeField] private Image PopupImage;
 
+    private DateTime _lastPopupTime;
+
     public void ShowCurrentPopup() {
         if (DataStore.instance.SelectedPopup == null) {
             PopupImage.enabled = false;
         } else {
-            Texture2D texture = DataStore.instance.SelectedPopup.PopupTexture2D;
-            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            var texture = DataStore.instance.SelectedPopup.PopupTexture2D;
+            var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
             PopupImage.sprite = sprite;
             PopupImage.enabled = true;
+            _lastPopupTime = DateTime.Now;
+        }
+    }
+
+    private void Update() {
+        if (DataStore.instance.SelectedPopup != null && Input.GetMouseButtonDown(0)) {
+            if (DateTime.Now - _lastPopupTime >= TimeSpan.FromSeconds(0.33f)) {
+                DataStore.instance.SelectedPopup = null;
+                ShowCurrentPopup();
+            }
         }
     }
     
